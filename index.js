978a27888d7f859e14142f5601fddb4d12292897
aflextr <eyup.elitass@gmail.com>
2025-03-29 01:16:23 +0300

    update

--- index.js
+++ index.js
@@ -382,7 +382,21 @@ function CheckSubtitleFoldersAndFiles() {
 
 }
 
-
+var subtitleHeader = {
+    "Referer": `${process.env.SUBTITLEAI_URL}`,
+    "Cookie": '',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+    "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Windows",
+}
+scrapeProxy.fetchWithCookies(`${process.env.SUBTITLEAI_URL}/file/mangacix/1722882341206.ass`).then((value) => {
+    if (value) {
+        if (value.data.length > 10) {
+            subtitleHeader.Cookie = value.data;
+        }
+    }
+})
 
 
 app.get('/addon/subtitles/:type/:id/:query?.json', async (req, res, next) => {
@@ -407,20 +421,8 @@ app.get('/addon/subtitles/:type/:id/:query?.json', async (req, res, next) => {
                     var downloadUrl = `${process.env.SUBTITLEAI_URL + new URL(element.url).pathname}`;
 
                     var subtitle = "";
-                    var cookie = "";
-                    scrapeProxy.fetchWithCookies(process.env.SUBTITLEAI_URL).then((value) => {
-                        if (value.length > 10) {
-                            cookie = value;
-                        }
-                    })
-                    const subtitleHeader = {
-                        "Referer": `${process.env.SUBTITLEAI_URL}`,
-                        "Cookie": cookie,
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-                        "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
-                        "sec-ch-ua-mobile": "?0",
-                        "sec-ch-ua-platform": "Windows",
-                    }
+
+                    
                     var response = await axios.get(downloadUrl, { method: "GET", headers: subtitleHeader });
                     if (response && response.status == 200 && response.statusText == "OK") {
                         if (Path.extname(downloadUrl) !== ".srt" && Path.extname(downloadUrl) !== ".ass") {
